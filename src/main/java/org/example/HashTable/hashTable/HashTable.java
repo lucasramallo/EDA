b09629967cd53f6f8b58e09cda731aec0a8f9957
lucasramallo <lucasdebrito.jd@gmail.com>
2024-07-31 21:36:55 -0300

    hashTable

--- src/main/java/org/example/HashTable/hashTable/HashTable.java
+++ src/main/java/org/example/HashTable/hashTable/HashTable.java
@@ -2,30 +2,64 @@ package org.example.HashTable.hashTable;
 
 public class HashTable {
     Aluno[] list;
-    int SIZE = 20;
+    int SIZE;
 
-    public HashTable() {
+    public HashTable(int initialSize) {
+        this.SIZE = initialSize;
         this.list = new Aluno[SIZE];
     }
 
     public void add(int key, Aluno aluno) {
         int hashedIndex = keyToHash(key);
+        boolean needResize = true;
 
-        if(this.list[hashedIndex] == null) {
-            this.list[hashedIndex] = aluno;
-        } else {
-            for(int i = hashedIndex; i < this.SIZE; i++) {
-
-                if(this.list[SIZE - 1] != null) resize(aluno);
-
-                if(this.list[i] == null) {
-                    this.list[i] = aluno;
-                    break;
-                }
-
+        for(int i = hashedIndex; i < this.SIZE; i++) {
+            if(this.list[i] == null || this.list[hashedIndex].getMatricula() == -1) {
+                this.list[i] = aluno;
+                needResize = false;
+                break;
             }
         }
 
+        if(needResize) {
+            resize(aluno);
+        }
+    }
+
+    public String get(int key) {
+        Aluno aluno = getAluno(key);
+
+        if(aluno != null) {
+            return aluno.getNome();
+        }
+
+        return null;
+    }
+
+    private Aluno getAluno(int key) {
+        int hashedIndex = keyToHash(key);
+
+        for(int i = hashedIndex; i < this.SIZE; i++) {
+            Aluno aluno = list[i];
+
+            if(aluno == null) {
+                throw new RuntimeException("Valor não encontrado!");
+            }
+
+            if (aluno.getMatricula() == key) {
+                return aluno;
+            }
+        }
+
+        return null;
+    }
+
+    public void remove(int key) {
+        Aluno aluno = getAluno(key);
+        if(aluno != null) {
+            aluno.setMatricula(-1);
+            aluno.setNome(null);
+        }
     }
 
     public void resize(Aluno aluno) {
@@ -34,12 +68,13 @@ public class HashTable {
         this.list = new Aluno[SIZE * 2];
         this.SIZE = this.SIZE * 2;
 
-        for (int i = 0; i < halfSize - 1; i++) {
-            if(listCopy[i] != null) add(listCopy[i].getMatricula(), listCopy[i]);
+        for (int i = 0; i < halfSize; i++) {
+            if(listCopy[i] != null && listCopy[i].getMatricula() != -1) {
+                add(listCopy[i].getMatricula(), listCopy[i]);
+            }
         }
 
         add(aluno.getMatricula(), aluno);
-
     }
 
     private int keyToHash(int key) {
--- src/main/java/org/example/HashTable/hashTable/Main.java
+++ src/main/java/org/example/HashTable/hashTable/Main.java
@@ -2,40 +2,34 @@ package org.example.HashTable.hashTable;
 
 public class Main {
     public static void main(String[] args) {
-        HashTable hashTable = new HashTable();
-
-//        hashTable.add(202401, new Aluno(202401, "Lucas"));
-//        hashTable.add(202401, new Aluno(202401, "Pedro"));
-//        hashTable.add(202401, new Aluno(202401, "Maria"));
-//        hashTable.add(202024, new Aluno(202024, "Juliana"));
-//        hashTable.add(202025, new Aluno(202025, "Thiago"));
-//        hashTable.add(202026, new Aluno(202026, "Mariana"));
-//        hashTable.add(202027, new Aluno(202027, "Rafael"));
-//        hashTable.add(202028, new Aluno(202028, "Beatriz"));
-//        hashTable.add(202029, new Aluno(202029, "Fernanda"));
-//        hashTable.add(202030, new Aluno(202030, "Gabriel"));
-//        hashTable.add(202031, new Aluno(202031, "Aline"));
-//        hashTable.add(202032, new Aluno(202032, "Eduardo"));
-//        hashTable.add(202033, new Aluno(202033, "Camila"));
-//        hashTable.add(202034, new Aluno(202034, "Pedro"));
-//        hashTable.add(202035, new Aluno(202035, "Larissa"));
-//        hashTable.add(202036, new Aluno(202036, "Felipe"));
-//        hashTable.add(202037, new Aluno(202037, "Isabela"));
-//        hashTable.add(202038, new Aluno(202038, "Vinicius"));
-//        hashTable.add(202039, new Aluno(202039, "Tatiana"));
-//
-//        hashTable.add(202039, new Aluno(202039, "Mauricio"));
+        HashTable hashTable = new HashTable(5);
 
         hashTable.add(202401, new Aluno(202401, "Lucas"));
         hashTable.add(202401, new Aluno(202401, "Pedro"));
         hashTable.add(202401, new Aluno(202401, "Maria"));
+        hashTable.add(202024, new Aluno(202024, "Juliana"));
+        hashTable.add(202025, new Aluno(202025, "Thiago"));
+        hashTable.add(202026, new Aluno(202026, "Mariana"));
+        hashTable.add(202027, new Aluno(202027, "Rafael"));
+        hashTable.add(202028, new Aluno(202028, "Beatriz"));
+        hashTable.add(202029, new Aluno(202029, "Fernanda"));
+        hashTable.add(202030, new Aluno(202030, "Gabriel"));
+        hashTable.add(202031, new Aluno(202031, "Aline"));
+        hashTable.add(202032, new Aluno(202032, "Eduardo"));
+        hashTable.add(202033, new Aluno(202033, "Camila"));
+        hashTable.add(202034, new Aluno(202034, "Pedro"));
+        hashTable.add(202035, new Aluno(202035, "Larissa"));
+        hashTable.add(202036, new Aluno(202036, "Felipe"));
+        hashTable.add(202037, new Aluno(202037, "Isabela"));
+        hashTable.add(202038, new Aluno(202038, "Vinicius"));
+        hashTable.add(202039, new Aluno(202039, "Tatiana"));
+        hashTable.add(202040, new Aluno(202040, "Mauricio"));
 
-        hashTable.add(202035, new Aluno(202035, "x"));
-        hashTable.add(202035, new Aluno(202035, "y"));
-        hashTable.add(202035, new Aluno(202035, "z"));
-        hashTable.add(202035, new Aluno(202035, "a"));
-        hashTable.add(202035, new Aluno(202035, "b"));
-        hashTable.add(202035, new Aluno(202035, "c"));
+        hashTable.remove(202024); // juliana
+        hashTable.add(202024, new Aluno(202024, "Filipe"));
+        System.out.println(hashTable.get(202040));
+
+
         hashTable.print();
     }
 }
